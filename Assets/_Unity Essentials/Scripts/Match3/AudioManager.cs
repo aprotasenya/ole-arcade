e531a13d0572935a235bad7ffdf64f1a1ae31aa9
aprotasenya <alexzander.protasenya@gmail.com>
2024-11-05 13:38:24 +0200

    AudioManager, SoundEffectSet SO

---
+++ Assets/_Unity Essentials/Scripts/Match3/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Match3
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioManager : MonoBehaviour
+    {
+        [SerializeField] SoundEffectSet SFXSet;
+        [HideInInspector] public bool canPlaySounds = true;
+
+        AudioSource audioSource;
+
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayClick() => PlayOneShotIfCan(SFXSet.click);
+        public void PlaySelect() => PlayOneShotIfCan(SFXSet.selectGem);
+        public void PlayDeselect() => PlayOneShotIfCan(SFXSet.deselectGem);
+
+        public void PlayMatch() => PlayOneShotIfCan(SFXSet.match);
+        public void PlayNoMatch() => PlayOneShotIfCan(SFXSet.noMatch);
+
+        public void PlayPop() => PlayRandomPitch(SFXSet.gemPop);
+        public void PlayDrop() => PlayRandomPitch(SFXSet.gemDrop);
+        public void PlayCreate() => PlayRandomPitch(SFXSet.gemCreate);
+
+        void PlayRandomPitch (AudioClip clip)
+        {
+            audioSource.pitch = Random.Range(0.9f, 1.1f);
+            PlayOneShotIfCan(clip);
+            audioSource.pitch = 1f;
+        }
+
+        void PlayOneShotIfCan(AudioClip clip)
+        {
+            if (canPlaySounds && clip != null)
+            {
+                audioSource.PlayOneShot(clip);
+            }
+        }
+    }
+}
---
+++ Assets/_Unity Essentials/Scripts/Match3/SoundEffectSet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Match3
+{
+    [CreateAssetMenu(fileName = "SoundEffectSet", menuName = "Match3/SoundEffectSet")]
+    public class SoundEffectSet : ScriptableObject
+    {
+        public AudioClip click;
+        public AudioClip selectGem;
+        public AudioClip deselectGem;
+        public AudioClip match;
+        public AudioClip noMatch;
+        public AudioClip gemPop;
+        public AudioClip gemDrop;
+        public AudioClip gemCreate;
+
+    }
+}
