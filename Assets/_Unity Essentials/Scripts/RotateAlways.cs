4eb726852e3ed82f01cf28ae31265a42526fd8c2
aprotasenya <alexzander.protasenya@gmail.com>
2024-11-12 13:53:47 +0200

    RotateAlways separated to a component

---
+++ Assets/_Unity Essentials/Scripts/RotateAlways.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RotateAlways : MonoBehaviour
+{
+    [SerializeField] Vector3 rotationVector = new Vector3(0f, 0.5f, 0f);
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(rotationVector);
+    }
+
+}
+
+
+
