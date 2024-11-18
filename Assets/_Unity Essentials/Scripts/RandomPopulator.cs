30d6f9b87259ad328829e9a7f5dfca2e4061282d
aprotasenya <alexzander.protasenya@gmail.com>
2024-11-18 20:33:17 +0200

    populator of spots + prefabs & scenes for col.conf

---
+++ Assets/_Unity Essentials/Scripts/RandomPopulator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class RandomPopulator : MonoBehaviour
+{
+    [SerializeField] Transform[] sceneSpots;
+    [SerializeField] GameObject[] prefabs;
+    [SerializeField] bool destroySpotObjects = true;
+
+    public void Populate()
+    {
+        foreach (var spot in sceneSpots)
+        {
+            var position = spot.position;
+            var rotation = spot.rotation;
+            var parent = spot.parent;
+
+            Instantiate(prefabs[Random.Range(0, prefabs.Length)], position, rotation, parent);
+        }
+
+        if (destroySpotObjects)
+        {
+            foreach (var spot in sceneSpots)
+            {
+                Destroy(spot.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/_Unity Essentials/Scripts/SpotGizmo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SpotGizmo : MonoBehaviour
+{
+    [SerializeField] Color gizmoColor = Color.yellow;
+    [SerializeField, Range(0.01f, 1f)] float radius = 0.1f;
+    [SerializeField] bool drawWireSphere = false;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        if (drawWireSphere) Gizmos.DrawWireSphere(transform.position, radius);
+        if (!drawWireSphere) Gizmos.DrawSphere(transform.position, radius);
+        
+    }
+
+}
